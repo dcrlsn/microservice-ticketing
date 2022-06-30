fdcb6fd735262da8c1264b521d154769605bdd11
89209062+dcrlsn@users.noreply.github.com <c4r150n@gmail.com>
2022-06-29 20:10:23 -0400

    Auth service in a reasonable spot

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,7 +12,14 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -24,8 +32,11 @@ app.all('*', () => {
 
 app.use(errorHandler);
 
-const start = async () => {
+const init = async () => {
   try {
+    if (!process.env.JWT_KEY) {
+      throw new Error('JWT KEY NOT DEFINED')
+    }
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('Connected to MongoDB')
   } catch (err) {
@@ -35,4 +46,5 @@ const start = async () => {
     console.log('Listening on Port 3000');
   });
 }
-start();
+
+init();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -17,12 +19,8 @@ router.post(
       .isLength({ min: 8, max: 32 })
       .withMessage('Password must be between 8 and 32 characters')
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
 
     const { email, password } = req.body;
 
@@ -34,6 +32,18 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT && Store on Session
+    const userJWT = jwt.sign({
+      id: user.id,
+      email: user.email
+    },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJWT
+    };
 
     res.status(201).send(user);
   }
